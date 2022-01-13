113669016a5de66d3fe8fa8a73099bac2cc7ab75
System Administrator <samroberval@gmail.com>
2022-01-13 00:29:38 +0000

    Commande migration

---
+++ database/migrations/2022_01_13_000052_create_commandes_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommandesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('commandes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('utilisateur_id')
+            ->constrained()
+            ->onDelete('cascade');
+            $table->foreignId('service_id')
+            ->constrained()
+            ->onDelete('cascade');
+            $table->integer('prix')->unsigned();
+            $table->integer('quantite')->unsigned()->default(1);
+            $table->enum('status', ['non-verifie', 'appel', 'verifie', 'en-cours-visite', 'fin-visite', 'valide', 'annule', 'execution', 'termine'])->default('non-verifie');
+            $table->bigInteger('technicien_id')->nullable();
+            $table->bigInteger('prestataire_id')->nullable();
+            $table->boolean('has_product')->nullable()->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('commandes');
+    }
+}
